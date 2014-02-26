719853dedb33ea1c4ef10fc9a38d9310f6627c70
Christian Rammelmüller <leramleram@users.noreply.github.com>
2014-02-26 23:29:09 +0100

    Create globalsh.py

---
+++ globalsh.py
@@ -0,0 +1,27 @@
+# -*- coding: utf-8 -*-
+"""
+Created on Tue Feb 25 19:08:26 2014
+
+@author: christian
+"""
+
+global cap
+global lspinBox
+global rspinBox
+global uspinBox
+global dspinBox
+
+lspinBox = 100
+rspinBox = 100
+uspinBox = 100
+dspinBox = 100
+
+global progBarV
+progBarV = 0
+
+global updateBar
+
+global steptotake
+global stepdelay
+steptotake = 0
+stepdelay = 0
